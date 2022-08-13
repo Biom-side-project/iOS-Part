67210a4fd8bfa7f6b1b24337555a8487a3c6d0fc
did250 <gmldnjs2500@naver.com>
2022-08-14 01:38:24 +0900

    Feat(google-login)

--- Biom/Biom/AppDelegate.swift
+++ Biom/Biom/AppDelegate.swift
@@ -1,23 +1,32 @@
-//
-//  AppDelegate.swift
-//  Biom
-//
-//  Created by 오예진 on 2022/08/13.
-//
 
 import UIKit
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+           if error != nil || user == nil {
+             // Show the app's signed-out state.
+           } else {
+             // Show the app's signed-in state.
+           }
+         }
         sleep(1)
         return true
     }
 
+    func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        return false
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Biom/Biom/ViewController.swift
+++ Biom/Biom/ViewController.swift
@@ -1,18 +1,44 @@
 import UIKit
+import GoogleSignIn
 
 class ViewController: UIViewController {
 
+    let config = GIDConfiguration(clientID: "278092451086-e1ks4nnvq6p7bl79bqfnt66rgposucge.apps.googleusercontent.com")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if GIDSignIn.sharedInstance.currentUser != nil {
+            DispatchQueue.main.async {
+                let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
+                newvc?.modalPresentationStyle = .fullScreen
+                newvc?.modalTransitionStyle = .crossDissolve
+                self.present(newvc!, animated: true, completion: nil)
+            }
+        }
         
     }
 
     @IBAction func google(_ sender: UIButton) {
-        let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
-        newvc?.modalPresentationStyle = .fullScreen
-        newvc?.modalTransitionStyle = .crossDissolve
-        self.present(newvc!, animated: true, completion: nil)
+        
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { user, error in
+            guard error == nil else { return }
+            guard let user = user else { return }
+
+            user.authentication.do { authentication, error in
+                guard error == nil else { return }
+                guard let authentication = authentication else { return }
+
+                let idToken = authentication.accessToken
+                // Send ID token to backend (example below).
+                print("token!!!!")
+                print(idToken)
+                let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
+                newvc?.modalPresentationStyle = .fullScreen
+                newvc?.modalTransitionStyle = .crossDissolve
+                self.present(newvc!, animated: true, completion: nil)
+            }
+        }
     }
-    
 }
 

bf6251c22fd2cd0ebf39ebc4b7b4a4782b54b7a4
did250 <gmldnjs2500@naver.com>
2022-08-22 16:41:34 +0900

    Feat(getlocation)

--- Biom/Biom/ViewController.swift
+++ Biom/Biom/ViewController.swift
@@ -2,26 +2,97 @@ import UIKit
 import GoogleSignIn
 import SwiftJWT
 import Alamofire
+import CoreLocation
 
-class ViewController: UIViewController {
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let region: Region
+}
+
+// MARK: - Region
+struct Region: Codable {
+    let area2: Area
+}
+
+// MARK: - Area
+struct Area: Codable {
+    let name: String
+    
+}
+
+
+
+
+
+var latitude = 0.0
+var longitude = 0.0
+var place = ""
+
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
     let config = GIDConfiguration(clientID: "278092451086-e1ks4nnvq6p7bl79bqfnt66rgposucge.apps.googleusercontent.com")
-    
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // MARK: - login 정보 남아있으면 자동 로그인
-        if GIDSignIn.sharedInstance.currentUser != nil {
-            DispatchQueue.main.async {
-                let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
-                newvc?.modalPresentationStyle = .fullScreen
-                newvc?.modalTransitionStyle = .crossDissolve
-                self.present(newvc!, animated: true, completion: nil)
-            }
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+   
+        if CLLocationManager.locationServicesEnabled() {
+            print("ON")
+            locationManager.startUpdatingLocation()
         }
+        else {
+            print("OFF")
+        }
+        // MARK: - login 정보 남아있으면 자동 로그인
+//        if GIDSignIn.sharedInstance.currentUser != nil {
+//            DispatchQueue.main.async {
+//                let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
+//                newvc?.modalPresentationStyle = .fullScreen
+//                newvc?.modalTransitionStyle = .crossDissolve
+//                self.present(newvc!, animated: true, completion: nil)
+//            }
+//        }
     }
 
     @IBAction func google(_ sender: UIButton) {
-        // MARK: - Google login
+        self.GoogleLogin() { token in
+            self.sendToken(token)
+            let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
+            newvc?.modalPresentationStyle = .fullScreen
+            newvc?.modalTransitionStyle = .crossDissolve
+            self.present(newvc!, animated: true, completion: nil)
+        }
+    }
+    
+    // MARK: - Send authentication token to server
+    func sendToken(_ token : String) {
+        let url = "http://biom-backend.ap-northeast-2.elasticbeanstalk.com/api/v1/login/naver"
+        let query = ["token": token]
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            try request.httpBody = JSONSerialization.data(withJSONObject: query, options: [])
+        } catch {
+            print("http Body Error")
+        }
+        AF.request(request).responseString { response in
+            let value =  String(data: response.data!, encoding: .utf8)
+            print(value)
+        }
+    }
+    
+    // MARK: - Google login
+    func GoogleLogin(completion : @escaping (String) -> ()) {
         GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { user, error in
             guard error == nil else { return }
             guard let user = user else { return }
@@ -29,21 +100,45 @@ class ViewController: UIViewController {
                 guard error == nil else { return }
                 guard let authentication = authentication else { return }
                 let token = authentication.accessToken
-                
-                // MARK: - Send accesToken to server
-                let url = "http://logintest02-env.eba-r2vdfqkp.ap-northeast-2.elasticbeanstalk.com/api/v1/token-test"
-                let query = ["token": token]
-                var components = URLComponents()
-                components.queryItems = query.map { URLQueryItem(name: $0, value: $1) }
-                AF.request(url, parameters: query, headers:  nil).responseString { response in
-                    print(response)
-                }
-                
-                let newvc = self.storyboard?.instantiateViewController(withIdentifier: "Tabbar")
-                newvc?.modalPresentationStyle = .fullScreen
-                newvc?.modalTransitionStyle = .crossDissolve
-                self.present(newvc!, animated: true, completion: nil)
+                print(token)
+                completion(token)
             }
         }
     }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        print("updated")
+        if let location = locations.first {
+            latitude = location.coordinate.latitude
+            longitude = location.coordinate.longitude
+//            print("위도 ", location.coordinate.latitude)
+//            print("경도 ", location.coordinate.longitude)
+            getplace()
+        }
+    }
+    
+    
+    func getplace() {
+        let url = "https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc"
+        var querystring = String(longitude)
+        querystring += ","
+        querystring += String(latitude)
+        let query = ["request" : "coordsToaddr","coords" : querystring, "output" : "json"]
+        var components = URLComponents()
+        components.queryItems = query.map { URLQueryItem(name: $0, value: $1) }
+        AF.request(url, parameters: query, headers: ["X-NCP-APIGW-API-KEY-ID" : "ztps04ekoq", "X-NCP-APIGW-API-KEY" : "5vSlPaWyrhiTBAkFWyvuZ6pTg545122hkhmf1C65"]).responseString { response in
+            do {
+                let data = try JSONDecoder().decode(Welcome.self, from: response.value!.data(using: .utf8)!)
+                place = data.results[0].region.area2.name
+                print(place)
+            } catch {
+                print(error)
+            }
+        }
+    }
+   
+    func sendplace() {
+        
+    }
 }
+
